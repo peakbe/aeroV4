ef2903f58ea90bf91a913452cd0a8798d2421dea
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-06-26 14:01:38 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,18 @@
 /****************************************************
  * Utils.js — Fonctions utilitaires Cockpit IFR PRO+++
  ****************************************************/
+export function dmsToDecimal(dms) {
+  const parts = dms.split(" ");
+  const deg = parseFloat(parts[0]);
+  const min = parseFloat(parts[1]);
+  const sec = parseFloat(parts[2]);
+  const dir = parts[3];
+
+  let dec = deg + min / 60 + sec / 3600;
+  if (dir === "S" || dir === "W") dec = -dec;
+
+  return dec;
+}
 
 // Différence angulaire (0–180°)
 export function angleDiff(a, b) {
