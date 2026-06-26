04f745adb8c4519cf6093880ee4daef3745498e5
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-06-26 12:13:58 +0200

    Add map initialization and airport markers

---
+++ js/map.js
@@ -0,0 +1,15 @@
+import { airports } from "./config.js";
+
+export let map;
+
+export function initMap() {
+  map = L.map("map").setView([50.5, 4.7], 10);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 18
+  }).addTo(map);
+
+  Object.values(airports).forEach(ap => {
+    L.marker([ap.lat, ap.lon]).addTo(map).bindPopup(ap.name);
+  });
+}
