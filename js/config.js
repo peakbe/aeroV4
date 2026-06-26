f94da8ab7a16d3f3086b22be61e2ec4068891287
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-06-26 12:13:06 +0200

    Add airport data and API keys to config.js

---
+++ js/config.js
@@ -0,0 +1,25 @@
+export const AVWX_API_KEY = "ersegQzkf2Dfal-o26B4b5uzMrXBeHK2jOpOaY7nffc";
+export const AIRLABS_KEY = "04cb1c09-8abb-468a-95fa-ee90c3c2b651";
+
+export const airports = {
+  EBCI: {
+    icao: "EBCI",
+    name: "Charleroi",
+    lat: 50.4592,
+    lon: 4.4538,
+    runways: [
+      { name: "06", heading: 60 },
+      { name: "24", heading: 240 }
+    ]
+  },
+  EBLG: {
+    icao: "EBLG",
+    name: "Liège",
+    lat: 50.6374,
+    lon: 5.4432,
+    runways: [
+      { name: "04", heading: 40 },
+      { name: "22", heading: 220 }
+    ]
+  }
+};
