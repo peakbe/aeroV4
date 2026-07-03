71937b94b4d4205ac893427dce2ebd872db64b91
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-07-03 15:42:33 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,6 +8,8 @@ export const airports = {
   EBCI: {
     icao: "EBCI",
     name: "Brussels South Charleroi",
+    lat: 50.459,
+    lon: 4.453,
     runways: [
       { name: "24", heading: 240, lat: 50.459, lon: 4.453 },
       { name: "06", heading: 60,  lat: 50.459, lon: 4.453 }
@@ -17,9 +19,12 @@ export const airports = {
   EBLG: {
     icao: "EBLG",
     name: "Liège Airport",
+    lat: 50.637,
+    lon: 5.443,
     runways: [
       { name: "22", heading: 220, lat: 50.637, lon: 5.443 },
       { name: "04", heading: 40,  lat: 50.637, lon: 5.443 }
     ]
   }
 };
+
