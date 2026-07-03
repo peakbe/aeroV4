da82cd066cddb2d4053917a682e09127697b95d5
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-07-03 15:32:48 +0200

    Update airport names and runway details

--- js/config.js
+++ js/config.js
@@ -1,25 +1,25 @@
-export const AVWX_API_KEY = "ersegQzkf2Dfal-o26B4b5uzMrXBeHK2jOpOaY7nffc";
+/****************************************************
+ * CONFIG — Cockpit IFR PRO+++
+ ****************************************************/
+
 export const AIRLABS_KEY = "04cb1c09-8abb-468a-95fa-ee90c3c2b651";
 
 export const airports = {
   EBCI: {
     icao: "EBCI",
-    name: "Charleroi",
-    lat: 50.4592,
-    lon: 4.4538,
+    name: "Brussels South Charleroi",
     runways: [
-      { name: "06", heading: 60 },
-      { name: "24", heading: 240 }
+      { name: "24", heading: 240, lat: 50.459, lon: 4.453 },
+      { name: "06", heading: 60,  lat: 50.459, lon: 4.453 }
     ]
   },
+
   EBLG: {
     icao: "EBLG",
-    name: "Liège",
-    lat: 50.6374,
-    lon: 5.4432,
+    name: "Liège Airport",
     runways: [
-      { name: "04", heading: 40 },
-      { name: "22", heading: 220 }
+      { name: "22", heading: 220, lat: 50.637, lon: 5.443 },
+      { name: "04", heading: 40,  lat: 50.637, lon: 5.443 }
     ]
   }
 };
