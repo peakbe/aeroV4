972513505c925818cda266805fb7066e4ded90a2
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-07-06 12:25:16 +0200

    Import AIRLABS_API_KEY from config.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
  * APP.JS — Orchestrateur Cockpit IFR PRO+++
  ****************************************************/
 
+import { AIRLABS_API_KEY } from "./config.js";
 import { airports } from "./config.js";
 import { initMap, map, resetMapView, refreshILS } from "./map.js";
 import { fetchMetar, updateMetarUI, fetchTaf, updateTafUI, updateWindRose, initMetarSwitch } from "./metar.js";
