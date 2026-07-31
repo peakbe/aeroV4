3d1af00faf15cee80984ab52911c0eddffcb86f4
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-07-31 12:49:37 +0200

    Add AVWX API key to config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
  ****************************************************/
 
 export const AIRLABS_API_KEY = "04cb1c09-8abb-468a-95fa-ee90c3c2b651";
-
+export const AVWX_API_KEY = "ersegQzkf2Dfal-o26B4b5uzMrXBeHK2jOpOaY7nffc";
 export const airports = {
 
   /****************************************************
