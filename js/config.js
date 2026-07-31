1898d9f417fbe55f869808d42482b806272fd923
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-07-31 12:45:11 +0200

    Add AIRLABS_API_KEY to config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
  * CONFIG — Cockpit IFR Airbus PRO+++
  ****************************************************/
 
-export const AVWX_API_KEY = "ersegQzkf2Dfal-o26B4b5uzMrXBeHK2jOpOaY7nffc";
+export const AIRLABS_API_KEY = "04cb1c09-8abb-468a-95fa-ee90c3c2b651";
 
 export const airports = {
 
